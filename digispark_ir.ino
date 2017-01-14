4a6fcd5058b771df12ff57ebfd8d68664e8c0537
jonboe <jonas.boegard@gmail.com>
2017-01-14 15:26:26 +0100

    First version

---
+++ digispark_ir.ino
@@ -0,0 +1,16 @@
+#include <IRremote.h>
+
+// Instantiate IRremote
+// on a digikey (ATTiny) it uses Pin 1.
+IRsend irsend;
+
+void setup()
+{
+  irsend.enableIROut(38);
+}
+
+void loop()
+{
+  irsend.mark(1000);
+  irsend.space(1000);
+}
